4648f1e29190b221082a63c43b8fef0521496fb5
Débora Sousa <sousxdebora@gmail.com>
2023-05-10 14:47:38 -0300

    exercícios do dia 2, sem os comentários

--- src/02-controle-de-fluxo/exercícios.py/ex02.py
+++ src/02-controle-de-fluxo/exercícios.py/ex02.py
@@ -1,4 +1,4 @@
-notas = '1 2 3 4'
+notas = '1 2 3 4 5'
 listas = notas.split(' ')
 
 print(listas)
@@ -21,30 +21,4 @@ elif media >= 4:
 else:
     print('Reprovado')
 
-    # Teste de melhoramento:
-"""  notas = '0'
-while notas >= '0':
-    notas = input('Entre com as notas: ')
-listas = notas.split(' ')
-
-print(listas)
-print('------')
-
-reais = []
-for elemento in listas:
-    reais.append(float(elemento))
-print(reais)
-
-soma = 0
-for real in reais:
-    soma = soma + real
-    media = soma / len(reais)
-print('A média do aluno é:', media)
-if media >= 6:
-    print('Aluno aprovado')
-elif media >= 4:
-    print('Recuperação')
-else:
-    print('Reprovado')
-
- """
+   
--- src/02-controle-de-fluxo/exercícios.py/ex04.py
+++ src/02-controle-de-fluxo/exercícios.py/ex04.py
@@ -1,13 +1,3 @@
-""" print('Entre com o ID:')
-id = input('')
-if len(id) != 7:
-    print('ID inválido!')
-    print('Identificador informado: ', id)
-    print('Erros do identificador: não apesenta números inteiros entre 00')
-elif id[0] != 'B' or id[1] != 'R' or id[6] != 'X':
-    print('ID inválido!')
-else:
-    print('ID válido!') """
 
 print('Entre com o ID:')
 id = input('')

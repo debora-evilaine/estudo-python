35c2ac982b7cfc9d689e992a76f309814ca20f96
Débora Sousa <sousxdebora@gmail.com>
2023-05-17 20:48:56 -0300

    commit com mudanças

--- src/06-arquivos/exercicios/ex02.py
+++ src/06-arquivos/exercicios/ex02.py
@@ -8,15 +8,9 @@ def carregar_dados_alunos(arquivo):
     dados_totais=[]
     
     for dado in dados:
-        dados_totais.append({'prontuario': dado[0], 'nome': dado[1], 'email': dado[2]})
+        dados_totais.append({'codigo': dado[0], 'título': dado[1], 'responsável': dado[2]})
         return tuple(dados_totais)
     print(dados_totais)
             
-with open('arquivo_de_dados.txt', 'r', encoding = 'utf-8') as arquivo:
+with open('arquivo_projetos.txt', 'r', encoding='utf-8') as arquivo:
     carregar_dados_alunos(arquivo)
-
-
-
- 
-
-

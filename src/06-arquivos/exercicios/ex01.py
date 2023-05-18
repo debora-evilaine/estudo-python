238ec4390c762b3cf6581ec08ebc15452b542d48
Débora Sousa <sousxdebora@gmail.com>
2023-05-18 15:08:30 -0300

    exercicios 01 e 02 da semana 3 finalizadosgit status porém em forma de list

--- src/06-arquivos/exercicios/ex01.py
+++ src/06-arquivos/exercicios/ex01.py
@@ -2,18 +2,19 @@
 def carregar_dados_alunos(arquivo):
     dados = []
     for linha in arquivo.readlines():
+        linha = linha.strip()
         linha = linha.split(',')
         dados.append(linha)
-        print(linha)
-    dados_totais=[]
+    
+   
     
     for dado in dados:
+        dados_totais=[]
         dados_totais.append({'prontuario': dado[0], 'nome': dado[1], 'email': dado[2]})
-        return tuple(dados_totais)
-    print(dados_totais)
+        print(dados_totais)
             
-with open('/home/evildeb/estudo-python/src/06/arquivos/arquivo_de_dados.txt', 'r', encoding = 'utf-8') as dados_reais:
-    carregar_dados_alunos(dados_reais)
+with open('/home/evildeb/estudo-python/src/06-arquivos/arquivo_de_dados.txt', 'r', encoding = 'utf-8') as total:
+    carregar_dados_alunos(total)
 
 
 
--- src/06-arquivos/exercicios/ex02.py
+++ src/06-arquivos/exercicios/ex02.py
@@ -5,12 +5,12 @@ def carregar_dados_alunos(arquivo):
         linha = linha.split(',')
         dados.append(linha)
         
-    dados_totais=[]
+    
     
     for dado in dados:
+        dados_totais=[]
         dados_totais.append({'codigo': dado[0], 'título': dado[1], 'responsável': dado[2]})
-        return tuple(dados_totais)
-    print(dados_totais)
+        print(dados_totais)
             
-with open('arquivo_projetos.txt', 'r', encoding='utf-8') as arquivo_real:
+with open('/home/evildeb/estudo-python/src/06-arquivos/arquivo_projetos.txt', 'r', encoding='utf-8') as arquivo_real:
     carregar_dados_alunos(arquivo_real)

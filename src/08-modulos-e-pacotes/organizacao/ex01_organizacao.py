6fbec03e04bf5fc3448ab6d3408ebaef52bb4627
Débora Sousa <sousxdebora@gmail.com>
2023-05-20 19:04:41 -0300

    primeira organizacao finalizadagit status!

---
+++ src/08-modulos-e-pacotes/organizacao/ex01_organizacao.py
@@ -0,0 +1,21 @@
+# primeiro passo: criar classe aluno com os valores básicos
+# Nenhum dos atributos pode ser vazio ou nulos (utilizar propriedades).
+import ex01_util
+
+aluno1 = ex01_util.Aluno('SP0101', 'João da Silva', 'joao@email.com')
+aluno2 = ex01_util.Aluno('', 'Maria da Silva', 'maria@email.com')
+
+# comparando se os alunos são iguais ou diferentes.....
+
+if aluno1.prontuario == aluno2.prontuario:
+    print('Os alunos são a mesma pessoa!')
+    
+else: 
+    print(aluno1)       
+    print(aluno2)
+    
+    
+    
+    
+    
+    
